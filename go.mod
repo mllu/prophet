8b83fe3f55c9e4ec5fbf54f0c6fc5729f2264e4b
mllu <mornlyn@gmail.com>
2018-10-10 06:18:35 +0000

    add gin for webhook

--- go.mod
+++ go.mod
@@ -1,10 +1,22 @@
 module github.com/mllu/prophet
 
 require (
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
+	github.com/gin-contrib/sse v0.0.0-20170109093832-22d885f9ecc7 // indirect
+	github.com/gin-gonic/gin v1.3.0
+	github.com/golang/protobuf v1.2.0
 	github.com/gorilla/websocket v1.4.0 // indirect
 	github.com/kelseyhightower/envconfig v1.3.0
+	github.com/mattn/go-isatty v0.0.4 // indirect
 	github.com/mllu/dialogflow-go-client v0.0.0-20181009201626-8dbb7c07fd10
 	github.com/mllu/go.uuid v1.2.1 // indirect
 	github.com/mllu/slack v0.4.2
 	github.com/pkg/errors v0.8.0 // indirect
+	github.com/sirupsen/logrus v1.1.1
+	github.com/ugorji/go/codec v0.0.0-20180927125128-99ea80c8b19a // indirect
+	golang.org/x/net v0.0.0-20181005035420-146acd28ed58 // indirect
+	google.golang.org/genproto v0.0.0-20181004005441-af9cb2a35e7f
+	google.golang.org/grpc v1.15.0 // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+	gopkg.in/yaml.v2 v2.2.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,39 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/mllu/prophet/pkg/config"
 	"github.com/mllu/prophet/pkg/handler"
 	mySlack "github.com/mllu/prophet/pkg/slack"
 
+	"github.com/fvbock/endless"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/protobuf/jsonpb"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/mllu/slack"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/genproto/googleapis/cloud/dialogflow/v2"
 )
+
+func handleWebhook(c *gin.Context) {
+	var err error
+
+	wr := dialogflow.WebhookRequest{}
+	if err = jsonpb.Unmarshal(c.Request.Body, &wr); err != nil {
+		logrus.WithError(err).Error("Couldn't Unmarshal request to jsonpb")
+		c.Status(http.StatusBadRequest)
+		return
+	}
+	log.Printf("%v", wr)
+	fmt.Println("I got a webhook", wr.GetQueryResult().GetOutputContexts())
+}
 
 func main() {
 
@@ -25,15 +48,56 @@ func main() {
 	slackListener := mySlack.NewSlackListener(slackClient, env.BotID, env.ChannelID)
 	go slackListener.ListenAndResponse()
 
-	// Register handler to receive interactive message
-	// responses from slack (kicked by user action)
-	http.Handle("/interaction", handler.NewInteractionHandler(slackClient, env.VerificationToken))
+	/*
+		// Register handler to receive interactive message
+		// responses from slack (kicked by user action)
+		http.Handle("/interaction", handler.NewInteractionHandler(slackClient, env.VerificationToken))
+	*/
 
-	http.HandleFunc("/", handler.ChallengeHandler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	})
+	mux.HandleFunc("/interaction", handler.NewInteractionHandler(slackClient, env.VerificationToken).ServeHTTP)
 
-	log.Printf("[INFO] Server listening on :%s", env.Port)
-	if err := http.ListenAndServe(":"+env.Port, nil); err != nil {
-		log.Printf("[ERROR] %s", err)
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", env.Port),
+		Handler: mux,
 	}
 
+	stop := make(chan os.Signal)
+	signal.Notify(stop, os.Interrupt)
+
+	go func() {
+		log.Printf("[INFO] Default Server listening on :%s", env.Port)
+		if err := srv.ListenAndServe(); err != nil {
+			if err != http.ErrServerClosed {
+				log.Fatal(err)
+			}
+		}
+	}()
+
+	r := gin.Default()
+	r.POST("/webhook", handleWebhook)
+
+	log.Printf("[INFO] Gin Server listening on :%s", ":8080")
+	endless.ListenAndServe(":8080", r)
+	/*
+		if err = r.Run(":8080"); err != nil {
+			logrus.WithError(err).Fatal("Couldn't start server")
+		}
+	*/
+
+	<-stop
+
+	log.Println("shutting down ...\n")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Bye...\n")
 }
